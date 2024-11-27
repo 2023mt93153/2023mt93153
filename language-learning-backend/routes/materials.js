82e02c93a67278a6b8ccb1adab777f50b4b6161d
2023mt93153 <2023mt93153@wilp.bits-pilani.ac.in>
2024-11-27 23:26:01 +0530

    Delete language-learning-backend directory

--- language-learning-backend/routes/materials.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getMaterialsByLanguage } = require('../controllers/materialsController');
-
-// Route to fetch materials filtered by language
-router.get('/', getMaterialsByLanguage);
-
-module.exports = router;
